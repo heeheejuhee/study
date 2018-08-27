0cc29cff9fff7d1559fb8fc55ebcc8687c511ac2
Juhee Han <200859841@cit3-slm-01.acg.ac.nz>
2018-08-27 17:01:23 +1200

    Finished Fake website

--- js/TEST/FakeWebsiteTest/js/script.js
+++ js/TEST/FakeWebsiteTest/js/script.js
@@ -1,19 +1,16 @@
 document.addEventListener("DOMContentLoaded",function(){
-			var btn = document.querySelector('.menu-btn');
-			var menu = document.querySelector('.menu-container');
-			
-			btn.addEventListener('click',function(){
-				if(this.className.includes('active')){
-					this.className = this.className.replace(" active","");
-				} else {
-					this.className += " active"
-				}
-			btn.addEventListener('click',function(){
-				if(this.className = ('.menu-btn')){
-					menu.style.display = "none";
-				} else {
-					menu.style.display = "block";
-				}
-			})
-		})
+	var btn = document.querySelector('.menu-btn');
+	var menu = document.querySelector('.menu-container');
+	
+	btn.addEventListener('click',function(){
+		if(this.className.includes('active')){
+			this.className = this.className.replace(" active","");
+			menu.style.display = "block";
+		} else {
+			this.className += " active"
+			menu.style.display = "none";
+		}
+	})
+
+})
 
