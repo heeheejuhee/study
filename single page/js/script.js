18d0a3171416a0fb88c64d311dee2aa274645b57
heeheejuhee <JuheeHan@hanjuhuiui-iMac.local>
2018-09-20 22:58:52 +1200

    removed onclick

--- single page/js/script.js
+++ single page/js/script.js
@@ -1,9 +1,8 @@
 document.addEventListener('DOMContentLoaded',function(){
-	
+	var mobilebtn = document.querySelector('.mobile-btn');
+	mobilebtn.addEventListener('click',function(){
+		var nav = document.querySelector('.navigation');
+		nav.classList.toggle('active')
+	})
 })
 
-function toggleNavigation(){
-		var nav = document.querySelector('.navigation');
-
-		nav.classList.toggle('active')
-	}
