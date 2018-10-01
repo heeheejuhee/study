575245f2f0780cb74fc4e954f3463f536ef1bbc4
Seoyeon Cho <200862108@cit3-slm-01.acg.ac.nz>
2018-10-01 19:02:49 +1300

    Portfolio added

--- single page/js/script.js
+++ single page/js/script.js
@@ -7,42 +7,36 @@ document.addEventListener('DOMContentLoaded',function(){
 
 	//Move Carousel
 
-	var btns = document.querySelectorAll('.carousel-btn');
-	// var rbtn = document.querySelector('.btn-right');
+	var carouselBtn = document.querySelectorAll('.carousel-btn');
 	var carousel = document.querySelector('.carousel');
 	var slide = document.querySelectorAll('.slide');
+	var currentIndex = 0;
 
-	for(var i = 0; i< btns.length; i++){
-		if(this.className.contains('.btn-right')){
-			carousel.style.left = -500 + "px";
-		} else{
-			carousel.style.right = 500 + "px";
-		}
+	for(var i = 0; i< carouselBtn.length; i++){
 
+		carouselBtn[i].addEventListener('click',function(){
+			var movement = slide[0].offsetWidth;
+			
+			if(this.classList.contains('btn-right')){
+				if(currentIndex < slide.length-1){
+					currentIndex++;
+					carousel.style.left = -movement * currentIndex + "px";
+				}
+			} else {
+				if(currentIndex != 0) {
+					currentIndex--;
+					carousel.style.left = -movement * currentIndex + "px";
+				}
+			}
+		})
 	}
 
-	// for(var i = 0; i< btns.length; i++){
-
-	// 	btns[i].addEventListener('click',function(){
-	// 		var movement = 500;
-	// 		var currentIndex = (carousel.getAttribute('data-index') == null)? 1 : slides.getAttribute('data-index');
-
-	// 		if(this.className.includes('btn-right')){
-	// 		 	if(currentIndex != slide.length){
-	// 			 	carousel.style.left = -(movement * currentIndex) + "px";
-	// 			 	carousel.setAttribute('data-index', parseInt(currentIndex) + 1)
-	// 		 	}
-	// 		 } else {
-	// 		 	if(currentIndex != 1 ){
-	// 				carousel.style.left = (parseInt(carousel.style.left) + movement) + "px";
-	// 			 	carousel.setAttribute('data-index', parseInt(currentIndex) - 1)	
-	// 		 	}			 			
-	// 		 }
-
-
-	// 	})
-	// }
-
+	var thumbnail = document.querySelectorAll('.thumbnail');
+	console.log(thumbnail);
+	for(var i = 0; i < thumbnail.length; i++){
+		console.log(thumbnail[i].offsetWidth)
+		thumbnail[i].style.height = thumbnail[i].offsetWidth + "px";
+	};
 
 })
 
